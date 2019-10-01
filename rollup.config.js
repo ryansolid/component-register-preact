34517c21040a3f2e4e342b10098e4a6316df6012
Ryan Carniato <ryansolid@gmail.com>
2019-10-01 16:26:52 -0700

    switch to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
+import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: 'src/index.js',
-  output: {
+  input: 'src/index.ts',
+  output: [{
     file: 'lib/component-register-preact.js',
     format: 'cjs',
     exports: 'named'
-  },
-  external: ['preact', 'component-register'],
+  }, {
+    file: 'dist/component-register-preact.js',
+    format: 'es'
+  }],
   plugins: [
-    nodeResolve({ extensions: ['.js'] })
-  ]
+    nodeResolve({ extensions: ['.js', '.ts'] }),
+    babel({
+      extensions: ['.js', '.ts'],
+      presets: ["@babel/preset-typescript"],
+      exclude: 'node_modules/**'
+    })
+  ],
+  external: ['preact', 'component-register'],
 };
