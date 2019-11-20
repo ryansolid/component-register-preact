e39450dad37fca99dd63164db476b5669f5a0949
Ryan Carniato <ryansolid@gmail.com>
2019-11-19 16:02:39 -0800

    update types

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,47 @@
-import { register, ComponentType, Props, ComponentOptions } from 'component-register';
-import { h, render, ComponentType as PreactComponentType } from 'preact';
-import { PropsDefinition } from 'component-register/types/utils';
+import {
+  register,
+  ComponentType,
+  ComponentOptions,
+  PropsDefinitionInput
+} from "component-register";
+import { h, render, ComponentType as PreactComponentType } from "preact";
 
-function withPreact(Component: PreactComponentType): ComponentType {
-  return (props: Props, { element }: ComponentOptions) => {
+function withPreact<T>(Component: PreactComponentType): ComponentType<T> {
+  return (props: T, { element }: ComponentOptions) => {
     let mountEl = element.renderRoot(),
       preactRoot = render(h(Component, props), mountEl);
 
-    element.addReleaseCallback(() => render('', mountEl, preactRoot as any));
+    element.addReleaseCallback(() => render("", mountEl, preactRoot as any));
 
     element.addPropertyChangedCallback((name: string, value: any) => {
-      props[name] = value;
+      props[name as keyof T] = value;
       preactRoot = render(h(Component, props), mountEl, preactRoot as any);
     });
   };
 }
 
-function customElement(tag: string, ComponentType: ComponentType): (ComponentType: ComponentType) => any
-function customElement(tag: string, props: PropsDefinition, ComponentType: ComponentType): (ComponentType: ComponentType) => any
-function customElement(tag: string, props: any, ComponentType?: any): (ComponentType: ComponentType) => any {
+function customElement<T>(
+  tag: string,
+  ComponentType: ComponentType<T>
+): (ComponentType: ComponentType<T>) => any;
+function customElement<T>(
+  tag: string,
+  props: PropsDefinitionInput<T>,
+  ComponentType: ComponentType<T>
+): (ComponentType: ComponentType<T>) => any;
+function customElement<T>(
+  tag: string,
+  props: any,
+  ComponentType?: any
+): (ComponentType: ComponentType<T>) => any {
   if (arguments.length === 2) {
     ComponentType = props;
     props = {};
   }
-  return register(tag, props)(withPreact(ComponentType));
+  return register<T>(
+    tag,
+    props as PropsDefinitionInput<T>
+  )(withPreact(ComponentType));
 }
 
-export { customElement, withPreact }
+export { customElement, withPreact };
